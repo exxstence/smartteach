a334081099bd8b49b1108c87a45b16aff2b7669c
exn <117230748+exxstence@users.noreply.github.com>
2024-03-11 16:42:23 +0200

    Update home.php (linking webiste

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartTeach.cc</title>
 </head>
-<body>
-    
-</body>
+    <body>
+        <h1>Hello!</h1>
+    </body>
 </html>
